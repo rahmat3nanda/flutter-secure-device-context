2db21f989be48fd0f00c6d81f8a91d472cd971dc
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-03-24 11:24:12 +0700

    Update version to 1.0.0 and reformat iOS implementation.
    - Bump plugin version from 0.0.1 to 1.0.0 in `secure_device_context.podspec`.
    - Reformat `SecureDeviceContextPlugin.swift` to use standard 4-space indentation.

--- ios/Classes/SecureDeviceContextPlugin.swift
+++ ios/Classes/SecureDeviceContextPlugin.swift
@@ -2,18 +2,18 @@ import Flutter
 import UIKit
 
 public class SecureDeviceContextPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "secure_device_context", binaryMessenger: registrar.messenger())
-    let instance = SecureDeviceContextPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "secure_device_context", binaryMessenger: registrar.messenger())
+        let instance = SecureDeviceContextPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
